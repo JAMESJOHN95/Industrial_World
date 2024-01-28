d434f7922824e9faf69822a3897e3384d405b843
unknown <jamesjhn19@gmail.com>
2024-01-28 16:49:17 +0530

    fourth

--- calculation.js
+++ calculation.js
@@ -9,3 +9,33 @@ const calculation = (e,lengthId, weightId, totalId) => {
         alert("Please enter valid values for length and weight.");
     }
     };
+
+// //////////////////////////////////////////////////////////////////////////
+
+// contact
+
+const register = ()=>{
+    var nameinput = document.getElementById('nameinput')
+    var Phoneinput = document.getElementById('Phoneinput')
+    var Emailinput = document.getElementById('Emailinput')
+
+
+    if(nameinput.value === "" || Phoneinput.value=== "" || Emailinput.value === ""){
+    alert("Please fill the form completlt !!!!!")
+    }
+    else if(Phoneinput.value.length !== 10){
+        alert("Enter a valid mobile number")
+    }
+    else if (!isValidEmail(Emailinput.value)){
+        alert("Enter A Valid Email")
+    }
+    else{
+        alert("REGISTRATION SUCCESFULL")
+        window.location.replace ("index.html")    }
+   
+}
+function isValidEmail(email) {
+    // Regular expression for basic email validation
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
